6a55482496ec1c6dc135c20bf07e4a5a18281534
lucifer2355 <dgajjar999@gmail.com>
2020-08-11 20:09:24 +0530

    add materia ui & icons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 
+import Sidebar from "./components/Sidebar";
+
 function App() {
   return (
     <div className='app'>
-      <h1>Hello World</h1>
+      <Sidebar />
     </div>
   );
 }
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import TwitterIcon from "@material-ui/icons/Twitter";
+
+function Sidebar() {
+  return (
+    <div className='sidebar'>
+      <TwitterIcon />
+    </div>
+  );
+}
+
+export default Sidebar;
