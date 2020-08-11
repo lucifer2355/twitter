a7e44137200f33515ce58b843ac152cd3f03a1a4
lucifer2355 <dgajjar999@gmail.com>
2020-08-11 20:17:01 +0530

    add sidear option component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import TwitterIcon from "@material-ui/icons/Twitter";
 
+import "../style/sidebar.css";
+import SidebarOptions from "./SidebarOptions";
+
 function Sidebar() {
   return (
     <div className='sidebar'>
       <TwitterIcon />
+      <SidebarOptions />
     </div>
   );
 }
---
+++ src/components/SidebarOptions.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "../style/sidebarOption.css";
+
+const SidebarOptions = () => {
+  return <div className='sidebarOption'></div>;
+};
+
+export default SidebarOptions;
